296d39338181e58b09579583a6a08f6beac383a2
Stone-afk <1711865140@qq.com>
2024-08-23 20:40:20 +0800

    feat: add v5 balance

--- v5/loadbalance/random/picker.go
+++ v5/loadbalance/random/picker.go
@@ -1,14 +1,14 @@
-package roundrobin
+package random
 
 import (
 	"emicro/v5/loadbalance"
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/balancer/base"
 	"google.golang.org/grpc/resolver"
-	"sync"
+	"math/rand"
 )
 
-const RoundRobin = "ROUND_ROBIN"
+const Random = "RANDOM"
 
 var (
 	_ balancer.Picker    = (*Picker)(nil)
@@ -40,25 +40,19 @@ func (b *PickerBuilder) Build(info base.PickerBuildInfo) balancer.Picker {
 }
 
 func (b *PickerBuilder) Name() string {
-	return RoundRobin
+	return Random
 }
 
 type Picker struct {
-	cnt         uint64
+	length      int
 	connections []conn
-	mutex       sync.Mutex
 	filter      loadbalance.Filter
 }
 
 func (p *Picker) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
-	//It is theoretically feasible to use atomic operations instead of locks,
-	// But the final effect is not a strict polling, but a rough polling
-	//In this case, why not use random directly?
-	// cnt := atomic.AddUint32(&b.cnt, 1)
-	// index := cnt % b.length
-	// atomic.StoreUint32(&b.cnt, index)
-	p.mutex.Lock()
-	defer p.mutex.Unlock()
+	if len(p.connections) == 0 {
+		return balancer.PickResult{}, balancer.ErrNoSubConnAvailable
+	}
 	candidates := make([]conn, 0, len(p.connections))
 	for _, c := range p.connections {
 		if !p.filter(info, c.address) {
@@ -66,31 +60,9 @@ func (p *Picker) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
 		}
 		candidates = append(candidates, c)
 	}
-	if len(candidates) == 0 {
-		return balancer.PickResult{}, balancer.ErrNoSubConnAvailable
-	}
-	index := p.cnt % uint64(len(candidates))
-	p.cnt += 1
+	index := rand.Intn(len(candidates))
 	return balancer.PickResult{
 		SubConn: candidates[index].SubConn,
-		// Used to design a feedback load balancing strategy
-		Done: func(info balancer.DoneInfo) {
-			//It can be labeled as unhealthy
-			// if info. Err != nil {
-			// }
-
-			// This place is a magical place
-			// The effect is to adjust your load balancing strategy according to the call result
-			// If something goes wrong
-			// if info. Err != nil {
-			//Try to make the subConn unavailable or temporarily remove it
-			// }
-
-			// 实际上，这里你是要考虑如果调用失败，
-			// 会不会是客户端和服务端的网络不通，
-			// 按照道理来说，是需要将这个连不通的节点删除的
-			// 但是删除之后又要考虑一段时间之后加回来
-		},
 	}, nil
 }
 
