e710b39e86328ef8f923afce70dd68f4c13a9416
Stone-afk <1711865140@qq.com>
2024-09-04 10:50:41 +0800

    feat: add v5 balance

--- v5/example/registry/client/client_test.go
+++ v5/example/registry/client/client_test.go
@@ -1,1 +1,39 @@
 package client
+
+import (
+	"context"
+	v5 "emicro/v5"
+	"emicro/v5/example/proto/gen"
+	"emicro/v5/registry/etcd"
+	"encoding/json"
+	"github.com/stretchr/testify/require"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"testing"
+	"time"
+)
+
+func TestClient(t *testing.T) {
+	etcdClient, err := clientv3.New(clientv3.Config{
+		Endpoints: []string{"localhost:2379"},
+	})
+	require.NoError(t, err)
+	r, err := etcd.NewRegistry(etcdClient)
+	require.NoError(t, err)
+	client := v5.NewClient(v5.ClientWithInsecure(), v5.ClientWithRegistry(r, time.Second*3))
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	// 设置初始化连接的 ctx
+	conn, err := client.Dial(ctx, "user-service")
+	cancel()
+	require.NoError(t, err)
+	userClient := gen.NewUserServiceClient(conn)
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second*3)
+	resp, err := userClient.GetById(ctx, &gen.GetByIdReq{
+		Id: 12,
+	})
+	cancel()
+	require.NoError(t, err)
+	bs, err := json.Marshal(resp.User)
+	require.NoError(t, err)
+	t.Log(string(bs))
+}
--- v5/example/registry/server/server_test.go
+++ v5/example/registry/server/server_test.go
@@ -1,1 +1,44 @@
 package server
+
+import (
+	"context"
+	v5 "emicro/v5"
+	"emicro/v5/example/proto/gen"
+	"emicro/v5/registry/etcd"
+	"fmt"
+	"github.com/stretchr/testify/require"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+	etcdClient, err := clientv3.New(clientv3.Config{
+		Endpoints: []string{"localhost:2379"},
+	})
+	require.NoError(t, err)
+	r, err := etcd.NewRegistry(etcdClient)
+	require.NoError(t, err)
+	us := &UserServiceServer{}
+	server := v5.NewServer("user-service", v5.ServerWithRegistry(r))
+	require.NoError(t, err)
+	gen.RegisterUserServiceServer(server, us)
+	t.Log("开始启动服务器")
+	// 我在这里调用 Start 方法，就意味着 us 已经完全准备好了
+	if err = server.Start(":8081"); err != nil {
+		t.Log(err)
+	}
+}
+
+type UserServiceServer struct {
+	gen.UnimplementedUserServiceServer
+}
+
+func (u *UserServiceServer) GetById(ctx context.Context, req *gen.GetByIdReq) (*gen.GetByIdResp, error) {
+	fmt.Printf("user id: %d", req.Id)
+	return &gen.GetByIdResp{
+		User: &gen.User{
+			Id:     req.Id,
+			Status: 123,
+		},
+	}, nil
+}
