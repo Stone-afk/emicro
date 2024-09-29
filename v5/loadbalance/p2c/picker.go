cae1ddca481ac72c592a4443a36e5cdc4062e740
Stone-afk <1711865140@qq.com>
2024-09-29 22:54:11 +0800

    feat: add redis obs

--- v5/loadbalance/p2c/picker.go
+++ v5/loadbalance/p2c/picker.go
@@ -1,9 +1,13 @@
 package p2c
 
 import (
+	"emicro/internal/utils/xsync"
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/resolver"
 	"math"
+	"math/rand"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -29,7 +33,49 @@ var emptyPickResult balancer.PickResult
 
 type PickerBuilder struct{}
 
+type Picker struct {
+	conns   []*Conn
+	r       *rand.Rand
+	lock    sync.Mutex
+	stamp   *xsync.AtomicDuration
+	logFunc func(info string, args ...any)
+}
+
 type Conn struct {
 	balancer.SubConn
 	address resolver.Address
+	// client statistic data
+	// average call delay (used to save ewma value)
+	latency uint64
+	// request success number
+	success uint64
+	// the number of requests currently being processed by the node
+	inflight int64
+	// total number of requests
+	requests int64
+	// last request completion time, used to calculate ewma value
+	last int64
+	// last selected time point
+	pick int64
+}
+
+func (c *Conn) healthy() bool {
+	return atomic.LoadUint64(&c.success) > throttleSuccess
+}
+
+// load() calculates the load of the node
+// The formula for calculating the load is: load = Sqrt(ewma) * inflight;
+// Here's a simple explanation: ewma is equivalent to the average request time,
+// and inflight is the number of requests being processed by the current node,
+// which is roughly calculated by multiplying the network load of the current node
+func (c *Conn) load() int64 {
+	// Add 1 to avoid zero
+	latency := int64(math.Sqrt(float64(atomic.LoadUint64(&c.latency) + 1)))
+	load := latency * (atomic.LoadInt64(&c.inflight) + 1)
+	if load == 0 {
+		// penalty is the penalty value when there is no data when the node is just started.
+		// The default value is max int32
+		return penalty
+	}
+	return load
 }
