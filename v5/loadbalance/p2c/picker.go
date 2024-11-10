7660efff8447274d9d78a5b67c43aaeef943c538
Stone-afk <1711865140@qq.com>
2024-11-10 10:54:21 +0800

    fix: add comment

--- v5/loadbalance/p2c/picker.go
+++ v5/loadbalance/p2c/picker.go
@@ -13,15 +13,20 @@ import (
 	"time"
 )
 
+// This package is a client load balancer. The algorithm used is p2c+ewma;
+// p2c is the "power of two choices" algorithm,
+// ewma is the exponentially weighted moving average (reflects the average over a period of time).
 // 该包是一个客户端负载均衡器。使用的算法是 p2c+ewma；
 // p2c 是二选一算法（power of two choices）
 // ewma 是指数移动加权平均（反映一段时间内的平均值）；
 
 const (
+	// Name is the name of the p2c load balancing algorithm
 	// Name 是 p2c 负载均衡算法的名称
 	Name            = "p2c_ewma"
 	initSuccess     = 1000
 	throttleSuccess = initSuccess / 2
+	// If no statistical data is collected, we add a large latency penalty to this endpoint.
 	// 如果未收集到统计数据，我们会对该端点添加一个较大的延迟惩罚
 	penalty   = int64(math.MaxInt32)
 	forcePick = int64(time.Second)
@@ -82,19 +87,27 @@ func (p *Picker) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
 type Conn struct {
 	balancer.SubConn
 	address resolver.Address
+	// Client statistics
+	// Average call latency (used to store the ewma value)
 	// 客户端统计数据
 	// 平均调用延迟（用于保存 ewma 值）
 	latency uint64
+	// Number of successful requests
 	// 请求成功数
 	success uint64
+	// Number of requests currently being processed by the node
 	// 节点当前正在处理的请求数
 	inflight int64
+	// Total number of requests
 	// 请求总数
 	requests int64
+	// Time of last request completion, used to calculate the ewma value
 	// 上次请求完成时间，用于计算ewma值
 	last int64
+	// Time of last selection
 	// 上次选择的时间点
 	pick int64
+	// Node name
 	// 节点名称
 	name string
 }
@@ -102,6 +115,12 @@ type Conn struct {
 func (c *Conn) healthy() bool {
 	return atomic.LoadUint64(&c.success) > throttleSuccess
 }
+
+// load() calculates the load of the node
+// The load calculation formula is: load = Sqrt(ewma) * inflight;
+// Simple explanation: ewma is the equivalent of the average request time,
+// inflight is the number of requests currently being processed by the node,
+// roughly calculated by multiplying the network load of the current node.
 
 // load() 计算节点的负载
 // 负载计算公式为：load = Sqrt(ewma) * inflight；
@@ -115,6 +134,8 @@ func (c *Conn) load() int64 {
 	if load == 0 {
 		// penalty is the penalty value when there is no data when the node is just started.
 		// The default value is max int32
+		// penalty 是节点刚启动时没有数据时的惩罚值。
+		// 默认值为 int32 的最大值
 		return penalty
 	}
 	return load
